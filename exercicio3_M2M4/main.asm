838bc7dcd95ceeeb129267dbaada0ec21c7b9f67
Guilherme Braga <gbp1999@gmail.com>
2019-09-01 13:21:52 -0300

    Add files via upload

--- exercicio3_M2M4/main.asm
+++ exercicio3_M2M4/main.asm
@@ -31,35 +31,35 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 
 M2M4:
 			mov.b	0(R5), R8				; R8 = tamanho do vetor
-			add		#1, R5
+			add		#1, R5					; caminha no vetor
 			clr		R14
 			clr		R6
 			clr		R7
 
 loop_divisores:
 
-			mov.b	0(R5), R13
-			mov		#0x0002, R12
-			call	#udiv
-			cmp		#1, R4
-			jeq		aumenta_divisiveis_2
+			mov.b	0(R5), R13				; passa o numero atual pro R13
+			mov		#0x0002, R12			; sera dividido por 2
+			call	#udiv					; faz a divisao
+			cmp		#1, R4					; se eh divisivel, a flag retorna 1
+			jeq		aumenta_divisiveis_2	; incrementa R6
 
 dividir_por_4:
 
-			clr		R4
-			mov		#0x0004, R12
-			call	#udiv
-			cmp		#1, R4
-			jeq		aumenta_divisiveis_4
+			clr		R4						; limpa o ultimo resultado
+			mov		#0x0004, R12			; dividiremos por 4
+			call	#udiv					; faz a divisão
+			cmp		#1, R4					; ve a flag de retorno
+			jeq		aumenta_divisiveis_4	; incrementa R7
 
 proximo_passo_divisao:
 
-			clr		R4
-			add		#1, R5
-			add		#1, R14
+			clr		R4						; limpa a flag
+			add		#1, R5					; vai pro proximo endereço
+			add		#1, R14					; aumenta o contador de qnts numeros analisamos
 
-			cmp		R8, R14
-			jeq		fim_algoritmo
+			cmp		R8, R14					; if numeros analisados = numeros no vetor, acaba o programa
+			jeq		fim_algoritmo			; else, faz dnv
 			jl		loop_divisores
 
 aumenta_divisiveis_2:
@@ -72,9 +72,9 @@ aumenta_divisiveis_4:
                                             
 udiv:
 
-			push	R13					; quociente
-			push	R14
-			clr		R14
+			push	R13					; quociente, contexto dado pois ele sera suibtraido
+			push	R14					; resultado da divisao, n esta sendo analisado logo vira contexto
+			clr		R14					; queremos apenas a flag R4
 			cmp		R12, R13
 			jl		fim_divisao
 
