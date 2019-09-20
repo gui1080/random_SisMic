e633c3965645162e454e82066950c3d11f04babd
Guilherme Braga <gbp1999@gmail.com>
2019-09-20 18:29:38 -0300

    Add files via upload

---
+++ mod2/exe1/main.c
@@ -0,0 +1,38 @@
+#include <msp430.h> 
+#include <stdint.h>
+
+void debounce(){
+
+    volatile uint16_t dt;
+    dt = 10000;
+    while(dt--);
+
+}
+
+
+int main(void)
+{
+    PM5CTL0 &= ~(LOCKLPM5);
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+	
+	P4DIR  &= ~(BIT1);          // botão 4.1
+	P4REN  |= BIT1;
+	P4OUT  |= BIT1;
+
+	P1DIR  |= BIT0;             // led
+	P1REN  &= ~(BIT0);
+	P1OUT &= ~(BIT0);
+
+	while(1){
+
+	    if(!(P4IN & BIT1)){
+	        P1OUT ^= BIT1;
+	    }
+	    else{
+	        P1OUT ^= BIT0;
+	    }
+
+	}
+
+	return 0;
+}
