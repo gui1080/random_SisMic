be4e7957f9bbe0ce5d958149c8cea2f54e0d16bf
Guilherme Braga <gbp1999@gmail.com>
2019-10-13 20:53:39 -0300

    progresso 2

---
+++ mod2/exe_9/main.c
@@ -0,0 +1,29 @@
+#include <msp430.h> 
+//#include <stdint.h>
+
+
+/**
+ * main.c
+ */
+int main(void)
+{
+    WDTCTL = WDTPW | WDTHOLD;   // stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;
+
+    P1OUT &= ~BIT0;
+    P1DIR |= BIT0;
+
+    TB0CTL = (TBSSEL__ACLK | ID__4 | MC__UP | TBCLR);
+
+    TB0CCR0 = 0x8000 - 1;
+
+    while(1){
+        while(!(TB0CCTL0 & CCIFG));
+        TB0CCTL0 &= ~CCIFG;
+
+        P1OUT ^= BIT0;
+    }
+
+
+    return 0;
+}
